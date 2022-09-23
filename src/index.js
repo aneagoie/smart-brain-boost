d2ec3d52cd060dc9a42ca0492b257f447b28c2cb
Andrei Neagoie <aneagoie@Andreis-MBP-2.lan>
2022-09-23 10:51:22 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import 'tachyons';
+import './index.css';
+
 
 const root = createRoot(document.getElementById('root'));
 
